f067e4b46f6112f2ae449928f2b9f103baaf75b1
kotaro.ono <kotaro.ono@optim.co.jp>
2024-10-10 19:53:47 +0900

    Changes

--- src/main/java/com/exmple/kyudo_app/model/location.java
+++ src/main/java/com/exmple/kyudo_app/model/location.java
@@ -5,20 +5,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import org.springframework.boot.SpringApplication;
+import org.yaml.snakeyaml.events.Event;
+import org.yaml.snakeyaml.tokens.Token;
 
 @Entity
 public class location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long locationId;
+    private Token.ID locationId;
     private String locationName;
     private String locationPoint;
 
-    public Long getLocationId() {
+    public Token.ID getLocationId() {
         return locationId;
     }
 
-    public void setLocationId(Long id) {
+    public void setLocationId(Token.ID id) {
         this.locationId = id;
     }
 
