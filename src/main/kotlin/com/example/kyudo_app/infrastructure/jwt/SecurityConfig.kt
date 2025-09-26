609d26de11b503d1609d4e9906dfba548cdf955d
kotaro.ono <kotaro.ono@optim.co.jp>
2025-09-27 02:41:23 +0900

    クロスサイトリクエストフォージェリ攻撃を受ける可能性あったためセッション管理を無効

--- src/main/kotlin/com/example/kyudo_app/infrastructure/jwt/SecurityConfig.kt
+++ src/main/kotlin/com/example/kyudo_app/infrastructure/jwt/SecurityConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
@@ -18,6 +19,9 @@ class SecurityConfig(
         http
             .csrf { it.disable() }
             .cors { } // CorsConfig で管理しているのでここは空でOK
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
             .authorizeHttpRequests { auth ->
                 auth
                     .requestMatchers("/auth/**", "/user/login", "/user/register").permitAll()
